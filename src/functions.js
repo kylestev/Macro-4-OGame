a52249921ee21d2a2c38db1ec64256e3691f842f
Kyle Stevenson <kyle.stevenson94@gmail.com>
2012-11-22 17:38:48 -0800

    time until built functions

--- src/functions.js
+++ src/functions.js
@@ -1,3 +1,26 @@
 function changePage(page) {
     window.location.href = 'http://' + document.domain + '/index.php?page=' + page;
 }
+
+function stringToTime(str) {
+    var time  = 0;
+    var parts = str.split(' ');
+
+    for (var i = parts.length - 1; i >= 0; i--) {
+        if (parts[i].substring(parts[i].length - 1) == 's') {
+            time += parseInt(parts[i].substring(0, parts[i].length - 1));
+        } else if (parts[i].substring(parts[i].length - 1) == 'm') {
+            time += 60 * parseInt(parts[i].substring(0, parts[i].length - 1));
+        } else if (parts[i].substring(parts[i].length - 1) == 'h') {
+            time += 3600 * parseInt(parts[i].substring(0, parts[i].length - 1));
+        } else if (parts[i].substring(parts[i].length - 1) == 'd') {
+            time += 86400 * parseInt(parts[i].substring(0, parts[i].length - 1));
+        }
+    };
+
+    return time;
+}
+
+function getTimeUntilBuilt() {
+    return stringToTime($('#timeLink span').text());
+}
