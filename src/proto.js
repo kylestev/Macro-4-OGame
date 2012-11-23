913c16a5f85b371b3475c17b0772cb792743a830
Kyle Stevenson <kyle.stevenson94@gmail.com>
2012-11-22 16:21:44 -0800

    removal of excess whitespace in proto

--- src/proto.js
+++ src/proto.js
@@ -1,6 +1,6 @@
 Function.prototype.method = function (name, func) {
     this.prototype[name] = func;
-    
+
     return this;
 };
 
@@ -14,18 +14,18 @@ Function.method('inherits', function (parent) {
         if (!(name in d)) {
             d[name] = 0;
         }
-        
+
         var f, r, t = d[name], v = parent.prototype;
         if (t) {
             while (t) {
                 v = v.constructor.prototype;
                 t -= 1;
             }
-        
+
             f = v[name];
         } else {
             f = p[name];
-        
+
             if (f == this[name]) {
                 f = v[name];
             }
@@ -34,7 +34,7 @@ Function.method('inherits', function (parent) {
         d[name] += 1;
         r = f.apply(this, Array.prototype.slice.apply(arguments, [1]));
         d[name] -= 1;
-    
+
         return r;
     });
 
