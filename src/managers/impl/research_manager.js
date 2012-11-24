9b7344ffa9ba3e005a5f516d8f0bcfd677a34356
Kyle Stevenson <kyle.stevenson94@gmail.com>
2012-11-23 17:35:38 -0800

    added level calculations for research items

--- src/managers/impl/research_manager.js
+++ src/managers/impl/research_manager.js
@@ -26,6 +26,128 @@ var res = [
     ['armor',        'tech',  'details111']
 ];
 
+var doubled = function (base_cost, next_level) {
+    return base_cost * Math.pow(2.0, next_level);
+};
+
+var costs = {
+    drive: {
+        combustion: {
+            metal: 400,
+            crystal: 0,
+            deuterium: 600,
+            cost: doubled
+        },
+        impulse: {
+            metal: 2000,
+            crystal: 4000,
+            deuterium: 600,
+            cost: doubled
+        },
+        hyperspace: {
+            metal: 10000,
+            crystal: 20000,
+            deuterium: 6000,
+            cost: doubled
+        }
+    },
+    tech: {
+        energy: {
+            metal: 0,
+            crystal: 800,
+            deuterium: 400,
+            energy: 0,
+            cost: doubled
+        },
+        laser: {
+            metal: 200,
+            crystal: 100,
+            deuterium: 0,
+            energy: 0,
+            cost: doubled
+        },
+        ion: {
+            metal: 1000,
+            crystal: 300,
+            deuterium: 100,
+            energy: 0,
+            cost: doubled
+        },
+        hyperspace: {
+            metal: 0,
+            crystal: 4000,
+            deuterium: 2000,
+            energy: 0,
+            cost: doubled
+        },
+        plasma: {
+            metal: 2000,
+            crystal: 4000,
+            deuterium: 1000,
+            energy: 0,
+            cost: doubled
+        },
+        espionage: {
+            metal: 200,
+            crystal: 1000,
+            deuterium: 200,
+            energy: 0,
+            cost: doubled
+        },
+        computer: {
+            metal: 0,
+            crystal: 400,
+            deuterium: 600,
+            energy: 0,
+            cost: doubled
+        },
+        astrophysics: {
+            metal: 4000,
+            crystal: 8000,
+            deuterium: 4000,
+            energy: 0,
+            cost: function (base_cost, next_level) {
+                return Math.round(base_cost * Math.pow(1.75, next_level - 1) / 100.0) * 100.0;
+            }
+        },
+        irn: {
+            metal: 240000,
+            crystal: 400000,
+            deuterium: 160000,
+            energy: 0,
+            cost: doubled
+        },
+        graviton: {
+            metal: 0,
+            crystal: 0,
+            deuterium: 0,
+            energy: 300000,
+            cost: doubled
+        },
+        weapons: {
+            metal: 800,
+            crystal: 200,
+            deuterium: 0,
+            energy: 0,
+            cost: doubled
+        },
+        shielding: {
+            metal: 200,
+            crystal: 600,
+            deuterium: 0,
+            energy: 0,
+            cost: doubled
+        },
+        armor: {
+            metal: 1000,
+            crystal: 0,
+            deuterium: 0,
+            energy: 0,
+            cost: doubled
+        }
+    }
+};
+
 for (var i = res.length - 1; i >= 0; i--) {
     ResearchManagerImpl.addEntity( res[i][0], res[i][1], res[i][2]);
 };
